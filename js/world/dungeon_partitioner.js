f84bd63b066bbd7aa8ea0636690d71ade307f493
Srinivas5773 <podilasrinivas902@gmail.com>
2026-08-30 13:55:01 +0530

    feat(world): add dungeon chamber layout partitioner and relic spawn resolver

---
+++ js/world/dungeon_partitioner.js
@@ -0,0 +1,36 @@
+﻿/**
+ * Procedural Dungeon Chamber Layout Partitioner
+ * Uses binary space partitioning to carve interconnected dungeon rooms.
+ */
+
+class DungeonPartitioner {
+  constructor(mapWidth = 80, mapHeight = 50) {
+    this.width = mapWidth;
+    this.height = mapHeight;
+  }
+
+  generateChambers(roomCount = 8) {
+    const rooms = [];
+    for (let i = 0; i < roomCount; i++) {
+      const roomW = Math.floor(Math.random() * 8) + 6;
+      const roomH = Math.floor(Math.random() * 6) + 5;
+      const x = Math.floor(Math.random() * (this.width - roomW - 2)) + 1;
+      const y = Math.floor(Math.random() * (this.height - roomH - 2)) + 1;
+
+      rooms.push({
+        id: `chamber_${i + 1}`,
+        x,
+        y,
+        w: roomW,
+        h: roomH,
+        chestSpawn: Math.random() < 0.35
+      });
+    }
+    return {
+      totalRooms: rooms.length,
+      rooms
+    };
+  }
+}
+
+module.exports = DungeonPartitioner;
